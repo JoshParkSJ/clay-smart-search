e2b2bee740dddbd4dbfccf5a98064727c9c29d3a
Joshua Park <50765702+JoshParkSJ@users.noreply.github.com>
2025-04-19 18:29:19 -0400

    create react app

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+import ExcelUI from './components/ExcelUI';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <ExcelUI />
+    </div>
+  );
+}
+
+export default App;
