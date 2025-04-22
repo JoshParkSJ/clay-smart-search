83980fbc26104fe11ebbbf3e3aab3043b825298b
Joshua Park <50765702+JoshParkSJ@users.noreply.github.com>
2025-04-21 20:20:38 -0400

    fix

--- apps/backend/api/index.ts
+++ apps/backend/api/index.ts
@@ -1,6 +1,6 @@
 import { VercelRequest, VercelResponse } from '@vercel/node';
 
-export default async function (req: VercelRequest, res: VercelResponse) {
+export default async function handler(req: VercelRequest, res: VercelResponse) {
   try {
     res.status(200).json({ message: 'Clay Smart Search API is running!' });
   } catch (error) {
